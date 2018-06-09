544b3949e75b3f5c5f21a693f76fd46c328c8f78
Kyle B. Johnson <kyle@wpninjas.com>
2018-06-08 22:08:22 -0400

    Initial commit.

---
+++ app/index.js
@@ -0,0 +1,46 @@
+import clock from "clock";
+import document from "document";
+import { preferences } from "user-settings";
+import { HeartRateSensor } from "heart-rate";
+import * as util from "../common/utils";
+
+const greeting = document.getElementById("greeting");
+greeting.text = "Hello, world!";
+
+// Update the clock every minute
+clock.granularity = "minutes";
+
+// Get a handle on the <text> element
+const myLabel = document.getElementById("myLabel");
+
+// Update the <text> element every tick with the current time
+clock.ontick = (evt) => {
+  let today = evt.date;
+  let hours = today.getHours();
+  if (preferences.clockDisplay === "12h") {
+    // 12h format
+    hours = hours % 12 || 12;
+  } else {
+    // 24h format
+    hours = util.zeroPad(hours);
+  }
+  let mins = util.zeroPad(today.getMinutes());
+  myLabel.text = `${hours}:${mins}`;
+}
+
+/* ------ */
+
+const hrLabel = document.getElementById("hrLabel");
+
+// Create a new instance of the HeartRateSensor object
+var hrm = new HeartRateSensor();
+
+// Declare an event handler that will be called every time a new HR value is received.
+hrm.onreading = function() {
+  // Peek the current sensor values
+  console.log("Current heart rate: " + hrm.heartRate);
+  hrLabel.text = "HR: " + hrm.heartRate;
+}
+
+// Begin monitoring the sensor
+hrm.start();
